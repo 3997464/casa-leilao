e2b65391abf3b66f8bf0269fd1624b799003cfde
3997464 <joelsonjoao18@gmail.com>
2026-01-14 19:44:12 -0300

    Criado banco de dados e tabela de produtos

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -22,7 +22,13 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+          conn = DriverManager.getConnection(
+    "jdbc:mysql://localhost:3306/uc11?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true",
+    "root",
+    "15161516"
+);
+
+
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
